b7b0db4e8391d8ba556131ab72f9188f69470185
mohosin07gmail <mohosin07@gmail.com>
2023-09-20 14:35:37 -0400

    format

--- src/gitHubDemo/HelloGitHub.java
+++ src/gitHubDemo/HelloGitHub.java
@@ -3,7 +3,8 @@ package gitHubDemo;
 public class HelloGitHub {
 
 	public static void main(String[] args) {
-		System.out.println("I know how to create a separate remote repository in Github and can push my project GithubDemo, without the help of anyone");
+		System.out.println(
+				"I know how to create a separate remote repository in Github and can push my project GithubDemo, without the help of anyone");
 
 	}
 
